92e09455a28fb83497af417f249b9394220d3033
yuyangw <39216513@qq.com>
2023-08-11 18:26:36 +0800

    feat(): UseSecondType实现，ExpandParameters初版实现

--- types/temp/index.ts
+++ types/temp/index.ts
@@ -1,3 +1,16 @@
-export type PickPartial<T, K extends keyof T> = Partial<Pick<T, K>> & Omit<T, K>
+/** 部分可选 */
+export type PickOptional<T, K extends keyof T> = Partial<Pick<T, K>> &
+  Omit<T, K>;
 
-export type OmitPartial<T, K extends keyof T> = Partial<Omit<T, K>> & Pick<T, K>
+/** 部分不可选 */
+export type OmitOptional<T, K extends keyof T> = Partial<Omit<T, K>> &
+  Pick<T, K>;
+
+export type UseSecondType<T, S = never> = S extends never ? T : S;
+
+export type ExpandParameters<
+  T extends (...arg: any) => any,
+  P extends any[]
+> = T extends (...arg: any) => any
+? [...Parameters<T>, ...P]
+: never;
