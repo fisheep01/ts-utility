f24d65c784b85812519a3b35e13fabbd8c9a9246
yuyangw <39216513@qq.com>
2023-08-17 13:37:59 +0800

    feat(): 完成ExpandParameters的实现

--- types/temp/index.ts
+++ types/temp/index.ts
@@ -8,9 +8,25 @@ export type OmitOptional<T, K extends keyof T> = Partial<Omit<T, K>> &
 
 export type UseSecondType<T, S = never> = S extends never ? T : S;
 
+type ExtractRequired<T extends any[]> = T extends []
+  ? []
+  : T extends [...infer _Rest, any]
+  ? T
+  : number extends T["length"]
+  ? []
+  : T extends [...infer _Rest, any?]
+  ? ExtractRequired<_Rest>
+  : [];
+
+type ExtractOptional<T extends any[]> = T extends [infer _First, ...infer _Rest]
+  ? _First extends undefined
+    ? T
+    : ExtractOptional<_Rest>
+  : T;
+
 export type ExpandParameters<
   T extends (...arg: any) => any,
   P extends any[]
 > = T extends (...arg: any) => any
-? [...Parameters<T>, ...P]
-: never;
+  ? [...ExtractRequired<Parameters<T>>, ...ExtractRequired<P>, ...ExtractOptional<Parameters<T>>,...ExtractOptional<P>]
+  : never;
